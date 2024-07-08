b3f19b0154b245612e2a343555707e7df2620198
Finn Plummer <canadienfinn@gmail.com>
2024-07-08 10:43:27 -0700

    disambiguate

--- src/card.rs
+++ src/card.rs
@@ -7,26 +7,26 @@ pub struct CardPlugin;
 
 impl Plugin for CardPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<CardID>()
+        app.init_resource::<NextCardID>()
             .add_systems(Startup, setup);
     }
 }
 
 fn setup(
-    mut card_id: ResMut<CardID>,
+    mut next_card_id: ResMut<NextCardID>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     for i in -2..3 {
         let posn = Vec2::new(i as f32 * 100., 0.);
-        spawn_card(card_id.reborrow(), commands.reborrow(), meshes.reborrow(), materials.reborrow(), posn);
+        spawn_card(next_card_id.reborrow(), commands.reborrow(), meshes.reborrow(), materials.reborrow(), posn);
     }
 
 }
 
 #[derive(Resource, Debug, Default)]
-struct CardID(u16);
+struct NextCardID(u16);
 
 #[derive(Component, Debug)]
 struct Card {
@@ -43,20 +43,20 @@ const CARD_WIDTH: f32 = 63.;
 const CARD_HEIGHT_SCALE: f32 = 1.4;
 
 fn spawn_card(
-    mut card_id: Mut<CardID>,
+    mut next_card_id: Mut<NextCardID>,
     mut commands: Commands,
     mut meshes: Mut<Assets<Mesh>>,
     mut materials: Mut<Assets<ColorMaterial>>,
     posn: Vec2
 ) {
-    info!("{:?} at {:?}", card_id, posn);
+    info!("{:?} at {:?}", next_card_id, posn);
     let shape = Mesh2dHandle(meshes.add(
             Rectangle::new(CARD_WIDTH, CARD_WIDTH * CARD_HEIGHT_SCALE)));
     let color = Color::srgb(139., 69., 19.);
     let mat = materials.add(color);
     commands.spawn(CardBundle {
         card: Card {
-            id: card_id.0,
+            id: next_card_id.0,
         },
         mesh: MaterialMesh2dBundle {
             mesh: shape,
@@ -67,5 +67,5 @@ fn spawn_card(
     });
 
     // increment the id for uniqueness
-    card_id.0 = card_id.0 + 1;
+    next_card_id.0 = next_card_id.0 + 1;
 }
